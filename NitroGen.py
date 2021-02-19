72ee4114bd1bd89f7fd92c15e682d8b33c822718
Nomade7 <74863556+Nomade7@users.noreply.github.com>
2021-02-19 11:27:05 +0100

    Create NitroGen.py

---
+++ NitroGen.py
@@ -0,0 +1,11 @@
+import random, string
+
+amount = int(input('Enter how many Nitro you want :'))
+value = 1
+while value <= amount:
+    code = "https://discord.gift/" + ('').join(random.choices(string.ascii_letters + string.digits, k=16))
+    f = open('Codes.txt', "a+")
+    f.write(f'{code}\n')
+    f.close()
+    print(f'[Code Nitro:] {code}')
+    value += 1
